17d072607c17b06a14e74477d830ceea5c94fc52
MurrayChenMBP2023 <murraychenmbp2023@SAMurrayChenMBP2023.local>
2024-01-25 11:57:02 +0800

    init

---
+++ src/worker.js
@@ -0,0 +1,19 @@
+// worker.js
+const fibonacci = (n) => {
+  console.log(n);
+  if (n <= 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+// event listener
+// 在 worker 程式中 self 指的是 worker 本身的 instance
+self.onmessage = (e) => {
+  const number = e.data;
+  const result = fibonacci(number);
+  self.postMessage(result);
+};
